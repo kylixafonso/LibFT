1ec60bfa7874fb7724c974cb38011c9788d29ba3
kylixalex <kylixsilva@gmail.com>
2021-08-30 17:19:37 +0000

    Add strcpy

---
+++ ft_strcpy.c
@@ -0,0 +1,17 @@
+char	*ft_strcpy(char *dest, const char *src)
+{
+	int	i;
+
+	i = 0;
+	while (src[i])
+	{
+		dest[i] = src[i];
+		i++;
+	}
+	while (dest[i])
+	{
+		dest[i] = '\0';
+		i++;
+	}
+	return (dest);
+}
