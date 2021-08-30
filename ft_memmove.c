a93940631811941df8db54a5ce53bbfe2cf51ec9
kylixalex <kylixsilva@gmail.com>
2021-08-30 17:21:40 +0000

    Fix error in memmove where the last strcpy was [dest, src] instead of [dest, temp]

--- ft_memmove.c
+++ ft_memmove.c
@@ -7,9 +7,12 @@ void	*ft_memmove(void *dest, const void *src, size_t n)
 	unsigned char	*temp;
 
 	temp = malloc(sizeof(unsigned char) * (n + 1));
-	ft_memcpy(temp, src, n);
-	temp[n] = '\0';
-	ft_memcpy(dest, src, n);
-	free(temp);
+	if (temp)
+	{
+		ft_memcpy(temp, src, n);
+		temp[n] = '\0';
+		ft_memcpy(dest, temp, n);
+		free(temp);
+	}
 	return (dest);
 }
