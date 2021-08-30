07bf5f5afce805ccb2eb70859d257d1fd6aea796
kylixalex <kylixsilva@gmail.com>
2021-08-30 17:23:06 +0000

    Update libft.h

--- libft.h
+++ libft.h
@@ -1,4 +1,29 @@
 #ifndef LIBFT_H
 # define LIBFT_H
 
+# include <string.h>
+
+void		*ft_memset(void *s, int c, size_t n);
+void		ft_bzero(void *s, size_t n);
+void		*ft_memcpy(void *dest, const void *src, size_t n);
+void		*ft_memccpy(void *dest, const void *src, int c, size_t n);
+void		*ft_memmove(void *dest, const void *src, size_t n);
+void		*ft_memchr(const void *s, int c, size_t n);
+int			ft_memcmp(const void *s1, const void *s2, size_t n);
+size_t		ft_strlen(const char *s);
+char		*ft_strdup(const char *s);
+char		*ft_strcpy(char *dest, const char *src);
+char		*ft_strncpy(char *dest, const char *src, size_t n);
+
+int	ft_strcmp(const char *s1, const char *s2);
+
+int			ft_atoi(const char *nbptr);
+int			ft_isalpha(int c);
+int			ft_isdigit(int c);
+int			ft_isalnum(int c);
+int			ft_isascii(int c);
+int			ft_isprint(int c);
+int			ft_toupper(int c);
+int			ft_tolower(int c);
+
 #endif
