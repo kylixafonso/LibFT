7540ab64ddf623e8d04cc7d2a3d1586c06429d33
kylixalex <kylixsilva@gmail.com>
2021-09-01 11:07:44 +0000

    Add strnstr

---
+++ ft_strnstr.c
@@ -0,0 +1,17 @@
+#include "libft.h"
+
+char	*ft_strnstr(const char *haystack, const char *needle, size_t n)
+{
+	size_t	i;
+	size_t	needle_len;
+
+	i = 0;
+	needle_len = ft_strlen(needle);
+	while (haystack[i] && i + needle_len < n)
+	{
+		if (!strncmp(haystack, needle, needle_len))
+			return ((char *)haystack + i);
+		i++;
+	}
+	return (NULL);
+}
