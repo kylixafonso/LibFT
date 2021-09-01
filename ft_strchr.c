73025391f28f62d539a2af7005e28ae1726aff46
kylixalex <kylixsilva@gmail.com>
2021-09-01 13:14:24 +0000

    Fix strchr returning wrong pointer

--- ft_strchr.c
+++ ft_strchr.c
@@ -1,10 +1,13 @@
 #include <string.h>
+#include "libft.h"
 
 char	*ft_strchr(const char *s, int c)
 {
 	int	i;
 
 	i = 0;
+	if (c == 0)
+		return ((char *)s + ft_strlen(s));
 	while (s[i] || (s[i - 1] && i >= 1))
 	{
 		if (s[i] == (char) c)
