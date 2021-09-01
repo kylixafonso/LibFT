6e54e2b103779f21071e006f09a04eb495655a3d
kylixalex <kylixsilva@gmail.com>
2021-09-01 11:08:04 +0000

    Add strrchr

---
+++ ft_strrchr.c
@@ -0,0 +1,22 @@
+#include <string.h>
+#include "libft.h"
+
+char	*ft_strrchr(const char *s, int c)
+{
+	char	*occurence;
+	size_t	len;
+	int	i;
+
+	i = 0;
+	occurence = NULL;
+	len = ft_strlen(s);
+	if (c == 0)
+		return ((char *)s + len);
+	while (s[i])
+	{
+		if (s[i] == (char) c)
+			occurence = (char *)s + i;
+		i++;
+	}
+	return (occurence);
+}
