8844308f2a9914f56523fa0d342e8b611e103d67
Kylix Alexander da silva alves afonso <kyalexan@c1r3s1.42lisboa.com>
2021-10-27 19:05:40 +0100

    Fix strlcpy

--- ft_strlcpy.c
+++ ft_strlcpy.c
@@ -1,9 +1,21 @@
 #include <string.h>
+#include "libft.h"
 
-size_t ft_strlcpy(char *dst, const char *src, size_t size)
+size_t	ft_strlcpy(char *dst, const char *src, size_t size)
 {
-	(void) dst;
-	(void) src;
-	(void) size;
-	return (2);
+	size_t	i;
+
+	i = 0;
+	if (size > 0)
+	{
+		while (src[i] && i < (size - 1))
+		{
+			dst[i] = src[i];
+			i++;
+		}
+		dst[i] = 0;
+	}
+	while (src[i])
+		i++;
+	return (i);
 }
