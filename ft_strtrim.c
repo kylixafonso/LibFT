22f7e27f58a46622b66caa315a71e1d780732d00
Kylix Alexander da silva alves afonso <kyalexan@c1r3s1.42lisboa.com>
2021-10-27 19:06:41 +0100

    Fix strtrim

--- ft_strtrim.c
+++ ft_strtrim.c
@@ -1,8 +1,41 @@
 #include <string.h>
+#include <stdlib.h>
+#include "libft.h"
+
+static int	in_set(char c, char const *set)
+{
+	int	i;
+
+	i = 0;
+	while (set[i])
+	{
+		if (c == set[i])
+			return (1);
+		i++;
+	}
+	return (0);
+}
 
 char	*ft_strtrim(char const *s1, char const *set)
 {
-	(void) s1;
-	(void) set;
-	return (NULL);
+	char		*str;
+	size_t		i;
+	size_t		j;
+	size_t		k;
+
+	j = ft_strlen(s1);
+	i = 0;
+	k = 0;
+	while (s1[i] && in_set(s1[i], set))
+		i++;
+	while (j > i && in_set(s1[j - 1], set))
+		j--;
+	str = (char *) malloc(sizeof(char *) * (j - i + 1));
+	if (str)
+		while (i < j)
+			str[k++] = s1[i++];
+	else
+		return (NULL);
+	str[k] = '\0';
+	return (str);
 }
