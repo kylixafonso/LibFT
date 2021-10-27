3513fa130349ed501c5b81cb1b7025cb3da2d2f7
Kylix Alexander da silva alves afonso <kyalexan@c1r3s1.42lisboa.com>
2021-10-27 19:04:02 +0100

    Add static helper function instead of calling unnecessary function

--- ft_strdup.c
+++ ft_strdup.c
@@ -2,7 +2,21 @@
 #include <stdlib.h>
 #include "libft.h"
 
-char	*strdup(const char *s)
+static char	*ft_strcpy(char *dest, const char *src)
+{
+	int	i;
+
+	i = 0;
+	while (src[i])
+	{
+		dest[i] = src[i];
+		i++;
+	}
+	dest[i] = '\0';
+	return (dest);
+}
+
+char	*ft_strdup(const char *s)
 {
 	int		len;
 	char	*new_string;
@@ -10,7 +24,9 @@ char	*strdup(const char *s)
 	len = ft_strlen(s);
 	new_string = malloc(sizeof(char) * (len + 1));
 	if (new_string)
+	{
 		ft_strcpy(new_string, s);
-	new_string[len] = '\0';
+		new_string[len] = '\0';
+	}
 	return (new_string);
 }
