1d06129a10a02352c0500564fd0d79eab3adecf1
Kylix Alexander da silva alves afonso <kyalexan@c1r3s1.42lisboa.com>
2021-10-27 19:02:23 +0100

    Change libft to only contain mandatory part

--- libft.h
+++ libft.h
@@ -2,6 +2,7 @@
 # define LIBFT_H
 
 # include <string.h>
+# include "libft_bonus.h"
 
 // C-LIB
 
@@ -15,8 +16,6 @@ void		*ft_calloc(size_t nmemb, size_t size);
 int			ft_memcmp(const void *s1, const void *s2, size_t n);
 size_t		ft_strlen(const char *s);
 char		*ft_strdup(const char *s);
-char		*ft_strcpy(char *dest, const char *src);
-char		*ft_strncpy(char *dest, const char *src, size_t n);
 size_t		ft_strlcpy(char *dst, const char *src, size_t size); 
 char		*ft_strcat(char *dest, const char *src);
 char		*ft_strncat(char *dest, const char *src, size_t n);
@@ -25,7 +24,6 @@ char		*ft_strchr(const char *s, int c);
 char		*ft_strrchr(const char *s, int c);
 char		*ft_strstr(const char *haystack, const char *needle);
 char		*ft_strnstr(const char *haystack, const char *needle, size_t n);
-int			ft_strcmp(const char *s1, const char *s2);
 int			ft_strncmp(const char *s1, const char *s2, size_t n);
 int			ft_atoi(const char *nbptr);
 int			ft_isalpha(int c);
@@ -42,6 +40,8 @@ char	*ft_substr(char const *s, unsigned int start, size_t len);
 char	*ft_strjoin(char const *s1, char const *s2);
 char	*ft_strtrim(char const *s1, char const *set);
 char	**ft_split(char const *s, char c);
+char	*ft_strmapi(char const *s, char (*f)(unsigned int, char));
+void	ft_striteri(char *s, void (*f)(unsigned int, char*));
 char *ft_itoa(int n);
 void ft_putchar_fd(char c, int fd);
 void ft_putstr_fd(char *s, int fd);
