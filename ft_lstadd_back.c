94bf5cf521e2a8052d92b4d30807dc65d59e654f
Kylix Alexander da silva alves afonso <kyalexan@c1r3s1.42lisboa.com>
2021-10-27 18:56:39 +0100

    Add lstadd_back

---
+++ ft_lstadd_back.c
@@ -0,0 +1,15 @@
+#include <stdlib.h>
+#include "libft_bonus.h"
+
+void	ft_lstadd_back(t_list **lst, t_list *new)
+{
+	t_list	*last;
+
+	if (*lst == NULL)
+		*lst = new;
+	else
+	{
+		last = ft_lstlast(*lst);
+		last->next = new;
+	}
+}
