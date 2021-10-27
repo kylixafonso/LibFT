50574001f6b49969f4cc2a8bc9b223bc3c122b8e
Kylix Alexander da silva alves afonso <kyalexan@c1r3s1.42lisboa.com>
2021-10-27 19:04:54 +0100

    Add static helper function instead of calling unnecessary function, fix

--- ft_substr.c
+++ ft_substr.c
@@ -1,12 +1,30 @@
 #include "libft.h"
 #include <stdlib.h>
 
+static char	*ft_strncpy(char *dest, const char *src, size_t n)
+{
+	size_t	i;
+
+	i = 0;
+	while (src[i] && i < n)
+	{
+		dest[i] = src[i];
+		i++;
+	}
+	while (dest[i] && i < n)
+	{
+		dest[i] = '\0';
+		i++;
+	}
+	return (dest);
+}
+
 char	*ft_substr(char const *s, unsigned int start, size_t len)
 {
 	char	*substr;
 
-	substr = malloc(sizeof(char) * (len + 1));
-	if (substr)
+	substr = ft_calloc((len + 1), sizeof(char));
+	if (substr && start < ft_strlen(s))
 		ft_strncpy(substr, s + start, len);
 	return (substr);
 }
