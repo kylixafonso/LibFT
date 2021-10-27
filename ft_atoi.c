5f7139a832950d16f3026da5a8e5fec16792d5d8
Kylix Alexander da silva alves afonso <kyalexan@c1r3s1.42lisboa.com>
2021-10-27 18:59:04 +0100

    Fix atoi

--- ft_atoi.c
+++ ft_atoi.c
@@ -29,6 +29,20 @@ static int	handle_sign(char c, int *i)
 	return (sign);
 }
 
+static int	ft_strcmp(const char *s1, const char *s2)
+{
+	int	i;
+
+	i = 0;
+	while (s1[i] || s2[i])
+	{
+		if (s1[i] != s2[i])
+			return ((unsigned char)s1[i] - (unsigned char)s2[i]);
+		i++;
+	}
+	return (0);
+}
+
 int	ft_atoi(const char *nptr)
 {
 	int	number;
