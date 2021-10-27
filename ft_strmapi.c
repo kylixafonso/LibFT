29301dd2fa1f99ea2c7c0534fd773a6278c5d2c4
Kylix Alexander da silva alves afonso <kyalexan@c1r3s1.42lisboa.com>
2021-10-27 18:56:10 +0100

    Add strmapi

---
+++ ft_strmapi.c
@@ -0,0 +1,27 @@
+#include <stdlib.h>
+#include "libft.h"
+
+char	*ft_strmapi(char const *s, char (*f)(unsigned int, char))
+{
+	char		*str;
+	size_t		len;
+	size_t		i;
+
+	if (!s || !f)
+		return (NULL);
+	i = 0;
+	len = ft_strlen(s);
+	str = (char *)malloc(sizeof(char) * (len + 1));
+	if (str)
+	{
+		while (s[i])
+		{
+			str[i] = f(i, s[i]);
+			i++;
+		}
+		str[i] = '\0';
+		return (str);
+	}
+	else
+		return (NULL);
+}
