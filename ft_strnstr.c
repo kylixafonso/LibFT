c263731901b2860d14b8ab2e2080710e6a074aff
Kylix Alexander da silva alves afonso <kyalexan@c1r3s1.42lisboa.com>
2021-10-27 19:06:18 +0100

    Fix strnstr, add protection

--- ft_strnstr.c
+++ ft_strnstr.c
@@ -3,14 +3,21 @@
 char	*ft_strnstr(const char *haystack, const char *needle, size_t n)
 {
 	size_t	i;
-	size_t	needle_len;
+	size_t	j;
 
+	if (!haystack || !needle)
+		return (NULL);
+	if (!needle || !needle[0])
+		return ((char*)haystack);
 	i = 0;
-	needle_len = ft_strlen(needle);
-	while (haystack[i] && i + needle_len < n)
+	while (haystack[i] && i < n)
 	{
-		if (!strncmp(haystack + i, needle, needle_len))
-			return ((char *)haystack + i);
+		j = 0;
+		while (haystack[i + j] && needle[j] &&
+				i + j < n && haystack[i + j] == needle[j])
+			j++;
+		if (!needle[j])
+			return ((char*)(haystack + i));
 		i++;
 	}
 	return (NULL);
